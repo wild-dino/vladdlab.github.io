a07b0052981b59a4bf5453faea79b34604b914c8
wild-dino <daritie8@gmail.com>
2021-03-14 00:40:03 +0300

    полный js

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
-// кажется тут должен быть код...
+let comment;
+let commentsArr;
+loadComments();
 
-// Подсказка №1: Содержимое тега textarea хранится в его свойстве value
+document.getElementById('comment-add').onclick = function () {
+    let commentText = document.getElementById ('comment');
+    comment = commentText.value;
+    commentText.value = '';
+    commentsArr.push({"comment": comment});
+    saveComments();
+    let commentSpace = document.querySelector('.comment__space');
+    commentSpace.innerHTML += `<p>${comment}</p>`;
+}
 
-// Подсказка №2: Не забывайте, что LocalStorage и SessionStorage могут хранить только строки в виде пар ключ/значение
+function saveComments () {
+    localStorage.setItem('comment', JSON.stringify(commentsArr));
+}
+
+function showComments () {
+    let commentSpace = document.querySelector('.comment__space');
+    let out = '';
+    if(commentsArr){
+        commentsArr.forEach(item => {
+            commentSpace.innerHTML += `<p>${item.comment}</p>`;
+        });
+    }
+}
+
+function loadComments() {
+
+    if (JSON.parse(localStorage.getItem('comment'))){
+        commentsArr = JSON.parse(localStorage.getItem('comment'));
+    }
+    else {
+        commentsArr = new Array();
+    }
+
+    showComments();
+}
+
